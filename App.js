a94e059c2228d64b090c59bd9efa7ad9da3e3e87
athnme <a.oezbanazi@gmail.com>
2020-11-26 11:10:10 +0100

    Add expo cli, init create native project, add styled components

--- App.js
+++ App.js
@@ -1,21 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  background-image: linear-gradient(180deg, #07211f 3.28%, #030d12 96.74%);
+`;
+
+//Typography
+
+const HeaderOne = styled.Text`
+  font-size: 32px;
+  line-height: 36px;
+  font-weight: 700;
+  letter-spacing: 0.25px;
+  color: #cecece;
+`;
+
+const HeaderTwo = styled.Text`
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 500;
+  letter-spacing: 0.25px;
+  color: #cecece;
+`;
+
+const HeaderThree = styled.Text`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 700;
+  letter-spacing: 0.25px;
+  color: #cecece;
+`;
+
+const HeaderFour = styled.Text`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+  letter-spacing: 0.25px;
+  color: #cecece;
+`;
+
+const HeaderFive = styled.Text`
+  font-size: 12px;
+  line-height: 18px;
+  font-weight: 700;
+  color: #cecece;
+`;
+
+const HeaderSix = styled.Text`
+  font-size: 12px;
+  line-height: 18px;
+  font-weight: 400;
+  color: #cecece;
+`;
+
+const Paragraph = styled.Text`
+  font-size: 10px;
+  line-height: 12px;
+  font-weight: 400;
+  color: #cecece;
+`;
+
+const ParagraphBold = styled.Text`
+  font-size: 10px;
+  line-height: 12px;
+  font-weight: 700;
+  color: #cecece;
+`;
+
+const Caption = styled.Text`
+  font-size: 10px;
+  line-height: 12px;
+  font-weight: 400;
+  color: #cecece;
+`;
+
+const CaptionBold = styled.Text`
+  font-size: 10px;
+  line-height: 12px;
+  font-weight: 700;
+  color: #cecece;
+`;
+
+const Placeholder = styled.Text`
+  font-size: 12px;
+  line-height: 24px;
+  font-weight: 400;
+  letter-spacing: 0.25px;
+  color: #cecece;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Container>
       <StatusBar style="auto" />
-    </View>
+
+      <HeaderOne>HeaderOne</HeaderOne>
+      <HeaderTwo>HeaderTwo</HeaderTwo>
+      <HeaderThree>HeaderThree</HeaderThree>
+      <HeaderFour>HeaderFour</HeaderFour>
+      <HeaderFive>HeaderFive</HeaderFive>
+      <HeaderSix>HeaderSix</HeaderSix>
+      <Paragraph>Paragraph</Paragraph>
+      <ParagraphBold>ParagraphBold</ParagraphBold>
+      <Caption>Caption</Caption>
+      <CaptionBold>CabtionBold</CaptionBold>
+      <Placeholder>Placeholder</Placeholder>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
